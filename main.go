91cd61452a6ce6c8b380e8a1f86cb7c2d8910cb8
berduk-dev <berdukm@mail.ru>
2025-07-23 02:37:53 +0300

    Update main.go
    Добавил доп. информацию к логам ошибок

--- main.go
+++ main.go
@@ -26,19 +26,19 @@ func main() {
 
 	marshalledPost, err := json.Marshal(post1)
 	if err != nil {
-		log.Println(err)
+		log.Printf("Ошибка при маршале: %w", err)
 	}
 
 	url := "https://httpbin.org/post"
 
 	resp, err := http.Post(url, "application/json", bytes.NewBuffer(marshalledPost))
 	if err != nil {
-		log.Println(err)
+		log.Printf("Ошибка при пост запросе: %w", err)
 	}
 
 	bytesResp, err := io.ReadAll(resp.Body)
 	if err != nil {
-		log.Println(err)
+		log.Printf("Ошибка при чтении респонса: %w", err)
 	}
 
 	fmt.Println(string(bytesResp))
