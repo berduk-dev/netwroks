e5ebf33a8d801233de9f780e79e87fad13d3d47e
berduk-dev <berdukm@mail.ru>
2025-07-26 18:00:02 +0300

    Update main.go

--- go-lessons/lesson-2/main.go
+++ go-lessons/lesson-2/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -12,14 +14,30 @@ type Post struct {
 	Body      string `json:"body"`
 }
 
-var posts []Post
-
 func main() {
 	r := gin.Default()
 	r.GET("/greet/:name", func(c *gin.Context) {
 		name := c.Param("name")
 
 		c.String(http.StatusOK, "Привет, %s", name)
+	})
+
+	r.GET("/calc", func(c *gin.Context) {
+		xStr := c.Query("x")
+		yStr := c.Query("y")
+
+		x, err := strconv.Atoi(xStr)
+		if err != nil {
+			log.Println("Ошибка в форматировании X:", err)
+		}
+
+		y, err := strconv.Atoi(yStr)
+		if err != nil {
+			log.Println("Ошибка в форматировании Y:", err)
+		}
+
+		sum := x + y
+		c.JSON(http.StatusOK, gin.H{"sum": sum})
 	})
 
 	r.Run()
